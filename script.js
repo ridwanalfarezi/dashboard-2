5b1dc7bac176fd6d8def97bb140d987f10163114
ridwanalfarezi <alfareziridwan@gmail.com>
2023-05-15 16:38:39 +0700

    add: sales report

--- script.js
+++ script.js
@@ -29,6 +29,19 @@ imgProfile.addEventListener("click", function () {
   dropdownProfile.classList.toggle("show");
 });
 
+// Menu
+
+const allMenu = document.querySelectorAll("main .content-data .head .menu");
+
+allMenu.forEach((item) => {
+  const icon = item.querySelector(".icon");
+  const menuLink = item.querySelector(".menu-link");
+
+  icon.addEventListener("click", function () {
+    menuLink.classList.toggle("show");
+  });
+});
+
 window.addEventListener("click", function (e) {
   if (e.target !== imgProfile) {
     if (e.target !== dropdownProfile) {
@@ -37,6 +50,19 @@ window.addEventListener("click", function (e) {
       }
     }
   }
+
+  allMenu.forEach((item) => {
+    const icon = item.querySelector(".icon");
+    const menuLink = item.querySelector(".menu-link");
+
+    if (e.target !== icon) {
+      if (e.target !== menuLink) {
+        if (menuLink.classList.contains("show")) {
+          menuLink.classList.remove("show");
+        }
+      }
+    }
+  });
 });
 
 // Progressbar
@@ -45,3 +71,47 @@ const allProgress = document.querySelectorAll("main .card .progress");
 allProgress.forEach((item) => {
   item.style.setProperty("--value", item.dataset.value);
 });
+
+// ApexChart
+var options = {
+  series: [
+    {
+      name: "series1",
+      data: [31, 40, 28, 51, 42, 109, 100],
+    },
+    {
+      name: "series2",
+      data: [11, 32, 45, 32, 34, 52, 41],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "area",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    type: "datetime",
+    categories: [
+      "2018-09-19T00:00:00.000Z",
+      "2018-09-19T01:30:00.000Z",
+      "2018-09-19T02:30:00.000Z",
+      "2018-09-19T03:30:00.000Z",
+      "2018-09-19T04:30:00.000Z",
+      "2018-09-19T05:30:00.000Z",
+      "2018-09-19T06:30:00.000Z",
+    ],
+  },
+  tooltip: {
+    x: {
+      format: "dd/MM/yy HH:mm",
+    },
+  },
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
