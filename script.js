3e54ceca1089c9880e5a1d071e7bbf4b3aa5545a
ridwanalfarezi <alfareziridwan@gmail.com>
2023-05-15 16:10:59 +0700

    add: card info data

--- script.js
+++ script.js
@@ -38,3 +38,10 @@ window.addEventListener("click", function (e) {
     }
   }
 });
+
+// Progressbar
+const allProgress = document.querySelectorAll("main .card .progress");
+
+allProgress.forEach((item) => {
+  item.style.setProperty("--value", item.dataset.value);
+});
